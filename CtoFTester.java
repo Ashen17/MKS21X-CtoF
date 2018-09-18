2bc7420fa8fa68cfaa97b75ad7d1459ee0fb2f37
Allen Shen <ashen1@stuy.edu>
2018-09-18 17:48:16 -0400

    Celcius to farenheit function

---
+++ CtoFTester.java
@@ -0,0 +1,11 @@
+public class CtoFTester{
+  public static void main(String[] args){
+
+  }
+  public static float celsiusToFahrenheit(float celcius){
+    return celcius * 1.8 + 32;
+  }
+  public static float farenheitToCelcius(flat farenheit){
+    return (farenheit - 32) / 1.8; 
+  }
+}
