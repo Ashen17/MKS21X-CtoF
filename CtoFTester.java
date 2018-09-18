b77170436912f254c0593a9f4095ddc9abe6c442
Allen Shen <ashen1@stuy.edu>
2018-09-18 18:26:05 -0400

    final version

--- CtoFTester.java
+++ CtoFTester.java
@@ -4,6 +4,12 @@ public class CtoFTester{
     System.out.println(celsiusToFahrenheit(7.0));
     System.out.println(celsiusToFahrenheit(30));
     System.out.println(celsiusToFahrenheit(30.0));
+    System.out.println(farenheitToCelcius(44.6));
+    System.out.println(farenheitToCelcius(80));
+    System.out.println(farenheitToCelcius(86));
+    System.out.println(farenheitToCelcius(-5));
+    System.out.println(farenheitToCelcius(celsiusToFahrenheit(5)));
+    System.out.println(celsiusToFahrenheit(farenheitToCelcius(5)));
   }
   public static double celsiusToFahrenheit(double celcius){
     return celcius * 1.8 + 32;
