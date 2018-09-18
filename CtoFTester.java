93743bd7cf5a1215132f96f8bac9fe8790422e60
Allen Shen <ashen1@stuy.edu>
2018-09-18 18:01:06 -0400

    committing the 4 tests

--- CtoFTester.java
+++ CtoFTester.java
@@ -1,11 +1,14 @@
 public class CtoFTester{
   public static void main(String[] args){
-
+    System.out.println(celsiusToFahrenheit(7));
+    System.out.println(celsiusToFahrenheit(7.0));
+    System.out.println(celsiusToFahrenheit(30));
+    System.out.println(celsiusToFahrenheit(30.0));
   }
-  public static float celsiusToFahrenheit(float celcius){
+  public static double celsiusToFahrenheit(double celcius){
     return celcius * 1.8 + 32;
   }
-  public static float farenheitToCelcius(flat farenheit){
-    return (farenheit - 32) / 1.8; 
+  public static double farenheitToCelcius(double farenheit){
+    return (farenheit - 32) / 1.8;
   }
 }
